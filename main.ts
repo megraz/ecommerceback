d8d30d7cbf77f3c0fe8ee9914b60f930b8534fc7
Egraz Marlene <marlene.egraz@gmail.com>
2017-10-24 16:55:05 +0200

    routes

--- main.ts
+++ main.ts
@@ -4,6 +4,12 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import {routerUser} from './routers/user-router';
 import {createConnection} from 'typeorm';
+import { routerProduct } from './routers/product-router';
+import { routerOrder } from './routers/order-router';
+import { routerContact } from './routers/contact-router';
+import { routerCategory } from './routers/category-router';
+import { routerAuthor } from './routers/author-router';
+import { routerAdmin } from './routers/admin-router';
 
 createConnection();
 const app = express();
@@ -13,5 +19,13 @@ app.use(bodyParser.urlencoded({
     extended:true
 }));
 
-app.use('/user', routerUser)
+app.use('/user', routerUser);
+app.use('/product', routerProduct);
+app.use('/order', routerOrder);
+app.use('/contact', routerContact);
+app.use('/category', routerCategory);
+app.use('/author', routerAuthor);
+app.use('/admin', routerAdmin);
+
+
 app.listen(3000, ()=>console.log('listening on port 3000'));
